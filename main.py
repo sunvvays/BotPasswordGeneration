7b6b01683a054f45a686e9bbbffda809e509ca41
sunways <sashaosk2006@gmail.com>
2024-04-29 18:09:33 +0700

    Renaming

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageDraw, ImageFont
 import telebot
 
 
-bot = telebot.TeleBot('6412448079:AAHCsLq2FF9_LsQmf_4NsGQ8lcB8t-DQWZY')
+bot = telebot.TeleBot('TOKEN')
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Привет! Выбери тип пароля:", reply_markup=get_keyboard_markup())
